35846f7b5a789cdf085aa9bdc864e039f46b1f3f
Arham659 <122161432+Arham659@users.noreply.github.com>
2023-02-23 16:37:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 //your code here
 
+var input=parseInt(document.getElementById("guess").value);
+ var numP=document.getElementById("num");
+var res=document.getElementById("respond");
+function randomNumGen(){
+	 //generate random number
+var	randomNum = Math.floor(Math.random()*41) - 20;
+	 //show random number
+	numP.innerText = randomNum;
+	
+  //check differnce and print
+	if(Math.abs(randomNum - input) <= 5){
+		res.innerText = "Hot";
+	}
+	else{
+		res.innerText = "Cold";
+	}
+}
